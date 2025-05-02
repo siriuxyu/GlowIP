4fd2f6bf7edab6c0d66049f3fdaca42f1ae0ed43
Siriux Yu <hengjia.y@outlook.com>
2025-05-02 16:06:36 +0800

    modified script template

--- scripts/run_new.sh
+++ scripts/run_new.sh
@@ -7,16 +7,17 @@ mode="$1"
 job_id="$2"
 dataset="$3"
 batchsize="$4"
+size="$5"
 job_name="${mode}_${job_id}"
 
 output_file="results/output_${job_name}.txt"
 error_file="results/errput_${job_name}.txt"
-log_file="${job_name}_log.txt"
+log_file="results/${job_name}_log.txt"
 script_template="scripts/job_template.sh"
 temp_script="scripts/job_${job_name}_temp.sh"
 
-# substitute 
-sed "s/{JOBNAME}/${job_name}/g; s/{DATASET}/${job_id}/g; s/{LOGFILE}/${log_file}/g" "$script_template" > "$temp_script"
+# Substitute 
+sed "s/{SIZE}/${size}/g; s/{BATCHSIZE}/${batchsize}/g; s/{JOBNAME}/${job_name}/g; s/{DATASET}/${datasize}/g; s/{LOGFILE}/${log_file}/g" "$script_template" > "$temp_script"
 
 # Remove old logs
 rm -f "$output_file" "$error_file" "$log_file"
