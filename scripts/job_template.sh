d3bdb060d5b7434c09b8cf6fd8fa4ff8a4efeb0a
Siriux Yu <hengjia.y@outlook.com>
2025-05-11 08:40:26 +0800

    scripts for ddp

--- scripts/job_template.sh
+++ scripts/job_template.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 #BSUB -J {JOBNAME}
 #BSUB -q gpuq
-#BSUB -n 10
-#BSUB -R "select[ngpus>=1] rusage[ngpus_shared=2]"
+#BSUB -n 4
+#BSUB -R "span[ptile=2]"
+#BSUB -gpu "num={NGPU}"
 #BSUB -o /gpfsdata/home/Zhaobo_hengjia21/GlowIP/results/output_{JOBNAME}.txt
 #BSUB -e /gpfsdata/home/Zhaobo_hengjia21/GlowIP/results/errput_{JOBNAME}.txt
 
+# ==== 环境变量设置 ====
 export PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/bin:$PATH
 export PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/envs/gpu122/bin:$PATH
 export LD_LIBRARY_PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/envs/gpu122/bin:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
 
+# ==== 环境与目录 ====
 cd /gpfsdata/home/Zhaobo_hengjia21/GlowIP
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate gpu122
 
-python train_glow.py -batchsize {BATCHSIZE} -dataset {DATASET} -size {SIZE} -job_id {JOBID} >> {LOGFILE}
+# ==== 设置 MASTER 地址 ====
+export MASTER_ADDR=$(host $(echo $LSB_HOSTS | awk '{print $1}') | awk '{print $1}')
+export MASTER_PORT=29500
 
+HOSTNAMES=($(echo $LSB_HOSTS | tr ' ' '\n' | uniq))
+NODE_RANK=0
+for i in "${!HOSTNAMES[@]}"; do
+  [[ "${HOSTNAMES[$i]}" == "$(hostname)" ]] && NODE_RANK=$i
+done
+
+# ==== 启动 torchrun（多GPU，多进程） ====
+torchrun \
+  --nproc_per_node=2 \
+  --nnodes={NGPU} \
+  --node_rank=$NODE_RANK \
+  --rdzv_backend=c10d \
+  --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
+  train_glow.py \
+  --batchsize {BATCHSIZE} \
+  --dataset {DATASET} \
+  --size {SIZE}
--- scripts/run_new.sh
+++ scripts/run_new.sh
@@ -1,5 +1,5 @@
-if [ $# -lt 4 ]; then
-  echo "Usage: $0 <mode> <dataset> <batchsize> <size>"
+if [ $# -lt 6 ]; then
+  echo "Usage: $0 <mode> <dataset> <batchsize> <size> <nnode> <job_id>"
   exit 1
 fi
 
@@ -7,7 +7,8 @@ mode="$1"
 dataset="$2"
 batchsize="$3"
 size="$4"
-job_id="$5"
+nnode="$5"
+job_id="$6"
 job_name="${mode}_${dataset}_${size}_${job_id}"
 
 output_file="results/output_${job_name}.txt"
@@ -18,8 +19,13 @@ temp_script="scripts/job_${job_name}_temp.sh"
 
 
 # Substitute placeholders in the job template
-sed "s#{SIZE}#${size}#g; s#{BATCHSIZE}#${batchsize}#g; s#{JOBNAME}#${job_name}#g; s#{DATASET}#${dataset}#g; s#{LOGFILE}#${log_file}#g; s#{JOBID}#${job_id}#g" "$script_template" > "$temp_script"
-
+sed "s#{SIZE}#${size}#g; \
+     s#{BATCHSIZE}#${batchsize}#g; \
+     s#{JOBNAME}#${job_name}#g; \
+     s#{DATASET}#${dataset}#g; \
+     s#{LOGFILE}#${log_file}#g; \
+     s#{JOBID}#${job_id}#g; \
+     s#{NGPU}#${nnode}#g" "$script_template" > "$temp_script"
 # Remove old logs
 rm -f "$output_file" "$error_file" "$log_file"
 
