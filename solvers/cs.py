7e3252c5af824745a5ca9678cf1337c1ace2103c
Siriux Yu <hengjia.y@outlook.com>
2025-05-14 11:37:54 +0800

    [solver] resize to args.size

--- solvers/cs.py
+++ solvers/cs.py
@@ -21,7 +21,7 @@ from scipy.linalg import null_space
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../data')))
-from nzpdata import NPZDataset
+from npzdata import NPZDataset
 
 import warnings
 warnings.filterwarnings("ignore")
