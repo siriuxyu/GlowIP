fd56bc25c015fd04b257b23a1efcc9d3296ebb92
Siriux Yu <hengjia.y@outlook.com>
2025-05-14 10:56:15 +0800

    start dealing with solvers

--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -14,8 +14,16 @@ job_name="${mode}_${dataset}_${size}_${job_id}"
 output_file="results/output_${job_name}.txt"
 error_file="results/errput_${job_name}.txt"
 log_file="results/${job_name}_log.txt"
-script_template="scripts/job_template.sh"
 temp_script="scripts/job_${job_name}_temp.sh"
+
+if [ "$mode" == "train" ]; then
+  script_template="scripts/job_template.sh"
+elif [ "$mode" == "solve" ]; then
+  script_template="scripts/solve_template.sh"
+else
+  echo "Invalid mode. Use 'train' or 'solve'."
+  exit 1
+fi
 
 
 # Substitute placeholders in the job template
