d705d9c4cdbe21d0e1fe0c9e9bf65d18cc118b5e
Siriux Yu <hengjia.y@outlook.com>
2025-05-18 11:55:59 +0800

    sample

---
+++ scripts/sample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -J sample
+#BSUB -q gpuq
+#BSUB -n 4
+#BSUB -R "select[ngpus>0] rusage[ngpus_shared=2]"
+#BSUB -o /gpfsdata/home/Zhaobo_hengjia21/GlowIP/results/output_sample.txt
+#BSUB -e /gpfsdata/home/Zhaobo_hengjia21/GlowIP/results/errput_sample.txt
+
+export PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/bin:$PATH
+export PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/envs/gpu122/bin:$PATH
+export LD_LIBRARY_PATH=/gpfsdata/home/Zhaobo_hengjia21/anaconda3/envs/gpu122/bin:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib64/:$LD_LIBRARY_PATH
+
+cd /gpfsdata/home/Zhaobo_hengjia21/GlowIP
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate gpu122
+
+
+python sample_glow.py -dataset BraTS -size 128 -device cpu -job_id 6
